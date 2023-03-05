c437397b9c5d0d5f54acdd59a804cbced485ce2d
mowalky <mowalky@outlook.com>
2023-03-04 20:42:49 -0600

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    let logical: bool = true;
+
+    let a_float: f64 = 1.0;
+
+    let an_integer = 23;
+
+    let default_float = 3.0;
+    let default_integer = 7;
+
+
+    let mut inferred_type  = 12;
+    inferred_type = 4294967296i64;
+
+    let _mutable = true;
+}
